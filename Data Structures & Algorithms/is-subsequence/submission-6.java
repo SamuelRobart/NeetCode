445aa6040684b35f8e0884c987eca6b1be567c2e
SamuelRobart <111070216+SamuelRobart@users.noreply.github.com>
2026-04-02 09:38:01 +0530

    Add: is-subsequence - submission-6

---
+++ Data Structures & Algorithms/is-subsequence/submission-6.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        if(s.length() == 0 ){
+            return true;
+        }
+
+        if( s == null || t == null){
+            return false;
+        }
+            
+
+        int j=0;
+
+        for(int i=0;i<t.length();i++){
+
+            if(j < s.length() && s.charAt(j) == t.charAt(i)){
+                
+                j++;
+
+                if(j==s.length())
+                    return true;
+            }
+        }
+
+        
+        return s.length() == j;
+    }
+}
