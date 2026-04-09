0959337b60923968f9797cd38eea0d16945d67b4
SamuelRobart <111070216+SamuelRobart@users.noreply.github.com>
2026-04-09 22:44:55 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {Map<Integer, Integer> map = new HashMap<>();
+            
+                    for (int i = 0; i < nums.length; i++) {
+                                int complement = target - nums[i];
+                                            
+                                                        // If the complement exists in the map, we've found our pair
+                                                                    if (map.containsKey(complement)) {
+                                                                                    return new int[] { map.get(complement), i };
+                                                                                                }
+                                                                                                            
+                                                                                                                        // Otherwise, store the current number and its index
+                                                                                                                                    map.put(nums[i], i);
+                                                                                                                                            }
+                                                                                                                                                    
+                                                                                                                                                            // This line is technically never reached based on problem constraints
+                                                                                                                                                                    throw new IllegalArgumentException("No two sum solution");
+                                                                                                                                                                        }
+        
+    
+}
